3918cec290868401de67f9c5d1c85657dfab6985
tataqt <tata_98@ukr.net>
2021-01-10 12:06:37 +0200

    Dto

---
+++ src/products/dto/update-product.dto.ts
@@ -0,0 +1,4 @@
+export class UprdateProductDto{
+    readonly title:string;
+    readonly price:number;
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,29 +1,32 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UprdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
     @Get()
-    getAll(): string{
+    getAll(): string {
         return 'getAll';
     }
 
     @Get(':id')
-    getById(@Param('id') id: string){
+    getById(@Param('id') id: string) {
         return `getById ${id}`;
     }
 
     @Post()
-    create(){
-
+    create(@Body() CreateProductDto: CreateProductDto): string {
+        return `Title = ${CreateProductDto.title}
+                Price = ${CreateProductDto.price}`;
     }
 
-    @Delete()
-    remove(){
-
+    @Delete(':id')
+    remove(@Param('id') id:string) {
+        return `Remove + ${id}`;
     }
 
-    @Put()
-    update(){
-        
+    @Put(':id')
+    update(@Body() updateProductDto: UprdateProductDto, @Param('id') id:string) {
+        return `Update + ${id}`;
     }
 }
